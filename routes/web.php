c98ba7089d1dd3023822598a2913a6b6f2be98f6
DanieleMorello <danielemorello@live.it>
2023-06-05 15:52:49 +0200

    Modify Route to middleware

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
